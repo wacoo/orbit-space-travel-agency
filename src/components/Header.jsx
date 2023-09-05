7d7fbf82c1e4b2fb28fc7c8ef552768e7b860a22
axcell-0 <121089024+axcell-0@users.noreply.github.com>
2023-09-05 10:28:32 +0100

    Add header title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,12 @@ import './Header.css';
 import Logo from '../img/logo.png';
 
 const Header = () => (
-  <header className="header" />
+  <header className="header" >
+    <div className="logo-title">
+      <img src={Logo} alt="Logo" />
+      <h1>rbit Space Travelers&apos; Hub</h1>
+    </div>
+  </header>
 );
 
 export default Header;
