fb26b99650a5e29d57c1b55f3231b9f9d033d3c7
axcell-0 <121089024+axcell-0@users.noreply.github.com>
2023-09-07 16:54:22 +0100

    Modify index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistStore } from 'redux-persist';
-import App from './App';
 import './index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 import store from './redux/store';
 
-const persister = persistStore(store);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate persistor={persister}>
+    <BrowserRouter>
+      <Provider store={store}>
         <App />
-      </PersistGate>
-    </Provider>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
